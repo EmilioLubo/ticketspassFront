03649349391b331eae6e9efa8b88f223d148aeef
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-12 19:29:27 -0300

    Agregada página concerts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { json, Route, Routes } from "react-router-dom";
 
 // Layout (Header/Footer)
 import Main from "./layouts/Main.jsx";
+import Concerts from "./pages/Concerts/Concerts.jsx";
 // Import of pages
 import Home from './pages/Home/Home.jsx'
 
@@ -13,6 +14,7 @@ export default function App() {
     <Main>
       <Routes>
         <Route path="/" element={<Home/>}/>
+        <Route path="/concerts" element={<Concerts />} />
       </Routes>
     </Main>
     </>
