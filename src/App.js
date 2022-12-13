768dadfa2493db510c47323c4aae7dd64e084ba8
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-13 15:46:31 -0300

    Conciertos

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@
 import { Route, Routes } from "react-router-dom";
 
 // Layout (Header/Footer)
-import Main from "./layouts/Main.jsx";
-import Concerts from "./pages/Concerts/Concerts.jsx";
+import Main from "./layouts/Main";
 // Import of pages
-import Home from './pages/Home/Home.jsx'
+import Home from './pages/Home/Home'
+import Concerts from "./pages/Concerts/Concerts";
 
 // Export to Index.js
 export default function App() {
   return (
     <>
-    <Main>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/concerts" element={<Concerts />} />
+        <Route path="/" element={<Main />} >
+          <Route index element={<Home/>}/>
+          <Route path="concerts" element={<Concerts />} />
+        </Route>
       </Routes>
-    </Main>
+    
     </>
   );
 }
