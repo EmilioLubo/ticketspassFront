6ec10f3c5dab3bb7108641487cf480ef2ad7675b
ALEJANDRO SANCHEZ <aleenetflix95@gmail.com>
2022-12-15 05:24:53 -0300

    Cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Main from "./layouts/Main";
 // Import of pages
 import Home from './pages/Home/Home'
 import Concerts from "./pages/Concerts/Concerts";
+import Cart from "./pages/Cart/Cart";
 
 // Export to Index.js
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Main />} >
           <Route index element={<Home/>}/>
           <Route path="concerts" element={<Concerts />} />
+          <Route path="cart" element={<Cart />} />
         </Route>
       </Routes>
     
