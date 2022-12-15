7c4d42324b42573fba5b66f28372fe54564050ae
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-15 14:00:08 -0300

    Edit concert

--- src/redux/actions/adminConcertsActions.js
+++ src/redux/actions/adminConcertsActions.js
@@ -16,8 +16,24 @@ const getInitialData = createAsyncThunk("getInitialData", async data => {
   }
 });
 
+const deleteConcert = createAsyncThunk("deleteConcert", async data => {
+  const { id, headers } = data;
+  try {
+    const res = await axios.delete(`${process.env.REACT_APP_URL || BASE_URL}/api/concerts/${id}`, headers);
+    return {
+      success: true,
+      id: res.data.id,
+      message: res.data.message,
+    };
+  } catch (error) {
+    let message = error.response ? error.response.data.message || error.response.data : error.message;
+    return { success: false, message };
+  }
+});
+
 const adminConcertsActions = {
   getInitialData,
+  deleteConcert
 };
 
 export default adminConcertsActions;
