4693beca6927d3e3afef4ac57760a3d907ffe110
ivan_gutierrez <ivan.gutierrezc92@gmail.com>
2022-12-15 20:17:33 -0300

    Funcionalidades de administración de lugares

--- src/pages/Adminvenues/AdminVenues.jsx
+++ src/pages/Adminvenues/AdminVenues.jsx
@@ -1,32 +1,71 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import { BASE_URL } from '../../api/url';
-import AdminTable from '../../utils/AdminTable/AdminTable';
+import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
+import adminVenuesActions from "../../redux/actions/AdminVenuesActions";
+import AdminTable from "../../utils/AdminTable/AdminTable";
 import "./AdminVenues.css";
 
 export default function AdminVenues() {
-  const [venues, setVenues] = useState([])
-  useEffect(() => {
-    getVenues();
-  }, [])
+  const dispatch = useDispatch();
+  const { token } = useSelector(store => store.user);
+  const { getInitialData, deleteVenue } = adminVenuesActions;
+  const { venues, message, loading } = useSelector(store => store.adminVenues);
 
-  const getVenues = async() => {
+  useEffect(() => {
+    dispatch(getInitialData());
+  }, []);
+
+  const deleteOnClick = async (id, name) => {
     try {
-      let res = await axios.get(`${BASE_URL}/api/venues`);
-      setVenues(res.data.response);
-    }catch(error) {
-      console.log(error.message)
+      let confirmation = await Swal.fire({
+        title: "Confirmation",
+        text: `Are you sure do you want to delete ${name}?`,
+        icon: "question",
+        showConfirmButton: true,
+        showDenyButton: true,
+        denyButtonColor: "blue",
+        confirmButtonText: "Yes",
+        confirmButtonColor: "red",
+      });
+      if (confirmation.isConfirmed) {
+        let headers = { headers: { Authorization: `Bearer ${token}` } };
+        let res = await dispatch(deleteVenue({ id, headers })).unwrap();
+        if (!res.success) {
+          Swal.fire({
+            title: "Error",
+            text: res.message,
+            icon: "error",
+          });
+        }
+      }
+    } catch (error) {
+      Swal.fire({
+        title: "Error",
+        text: error.message,
+        icon: "error",
+      });
     }
-  }
+  };
 
   return (
     <div>
-      <Link to="/admin/concerts/new">
-        <Button variant="success" className="mb-4">Nuevo Lugar</Button>
+      <Link to="/admin/venues/new">
+        <Button variant="success" className="mb-4">
+          Nuevo Lugar
+        </Button>
       </Link>
-      <AdminTable title="Lugares" collection={venues} editRoute="/admin/venues/edit/" deleteOnClick={() => {}} />
+      {loading ? (
+        <div className="d-flex justify-content-center">
+          <Spinner className="text-center" />
+        </div>
+      ) : venues.length > 0 ? (
+        <AdminTable title="Lugares" collection={venues} editRoute="/admin/venues/edit/" deleteOnClick={deleteOnClick} />
+      ) : (
+        <h2 className="text-center text-main">{message}</h2>
+      )}
     </div>
-  )
+  );
 }
